af67ea7f0d9aef6dd2f8c91938225cdcd3fd3494
yujieli <yujieli001@live.cn>
2016-01-28 14:09:57 +0800

    Update minvol.go

--- minvol.go
+++ minvol.go
@@ -1,9 +1,19 @@
-package main
-
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println("Hi")
-}
+ import (
+ 	"fmt"
+-	"net/http"
+ )
+ 
+-func helloHandler(w http.ResponseWriter, r *http.Request) {
+-	fmt.Fprintln(w, "Hello World from Go in minimal Docker container")
+-}
+-
+ func main() {
+-	http.HandleFunc("/", helloHandler)
+-
+-	fmt.Println("Started, serving at 8080")
+-	err := http.ListenAndServe(":8080", nil)
+-	if err != nil {
+-		panic("ListenAndServe: " + err.Error())
+-	}
++	fmt.Println("Hi")
+ }
